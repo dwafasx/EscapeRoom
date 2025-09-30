095b5a32031dea06cbdef4b569f91c7a997d9499
dwafasx <2517565989@qq.com>
2025-09-30 21:13:32 +0800

    upload jump

--- EscapeRoom/Assets/Scripts/RoleController.cs
+++ EscapeRoom/Assets/Scripts/RoleController.cs
@@ -1,21 +1,28 @@
+using System;
 using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    private Animator animator;
+    public Animator animator;
+    public CharacterController characterController;
+    [Header("移动设置")]
     public float walkSpeed = 2.0f;//,既是融合树Speed行走时数值，又是角色行走速度
     public float runSpeed = 5.0f;//同上
-
-    public float acceleration = 3f; // 融合树Speed变量改变时的加速度
-    public float deceleration = 3f; // 融合树Speed变量改变时的减速度
+    public float acceleration; // 融合树Speed变量改变时的加速度
+    public float deceleration; // 融合树Speed变量改变时的减速度
     private float currentSpeed = 0f;
-
     float herizontalMouseSum=0;    //鼠标水平移动累加
     float verticalMouseSum = 0;//鼠标垂直移动累加
-    float minCameraAngle=-60f;
-    float maxCameraAngle=60f;
-    public CharacterController characterController;
-
+    float minCameraAngle=-60f;//摄像机最小角度
+    float maxCameraAngle=60f;//摄像机最大角度
+    [Header("跳跃设置")]
+    public Transform groundCheck;//地面检测的空物体的位置
+    public float checkRadius =1.3f;//检测半径
+    public bool isGrounded=false;
+    public LayerMask groundMask;
+    public float gravity;
+    public float jumpForce;
+    private Vector3 velocity=Vector3.zero;
     void Start()
     {
         //锁定鼠标到屏幕中心并隐藏
@@ -24,15 +31,46 @@ public class BasicMovement : MonoBehaviour
 
         animator = GetComponent<Animator>();
         characterController=GetComponent<CharacterController>();
+        if(groundCheck == null)
+        {
+            groundCheck = transform;
+        }
     }
 
     void Update()
     {
+        GroundCheck();//地面检测
+        ApplyGravity();//应用重力
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+            Debug.Log(velocity.x+"  "+ velocity.y + "  "+velocity.z);
+        characterController.Move(velocity*0.01f);//角色跳跃
         Move();
-        roleCamera();
+        RoleCamera();
     }
 
+    private void Jump()
+    {
+        velocity.y =  Mathf.Sqrt(2 * gravity * jumpForce);
+    }
+    private void ApplyGravity()
+    {
+        if (!isGrounded){
+            velocity.y -= gravity * Time.deltaTime;
+        }
+    }
 
+    private void GroundCheck()
+    {
+        //isGrounded = characterController.isGrounded;
+        isGrounded=Physics.CheckSphere(groundCheck.position,checkRadius,groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+    }
 
     void Move()
     {
@@ -66,7 +104,7 @@ public class BasicMovement : MonoBehaviour
         characterController.Move(move*currentSpeed*Time.deltaTime);
 
     }
-    void roleCamera()
+    void RoleCamera()
     {
         float horizontal = Input.GetAxis("Mouse X");
         float vertical = Input.GetAxis("Mouse Y");
