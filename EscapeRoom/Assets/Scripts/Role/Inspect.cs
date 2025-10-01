6d58756fa4387a49949479d61df625c5dd76fd5f
dwafasx <2517565989@qq.com>
2025-10-01 17:32:44 +0800

    upload

--- EscapeRoom/Assets/Scripts/Role/Inspect.cs
+++ EscapeRoom/Assets/Scripts/Role/Inspect.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 //角色交互
 public class Inspect : MonoBehaviour
 {
-    public GameObject currentObject;
+    public GameObject currentObject;//当前物体，即射线检测到的tag为Inspectable的物体
     [Header("物体名称显示面板")]
-    public Canvas canvas;//InspectCanvas
+    public Canvas canvas;
+    public Image ObjectPanel;//显示物体信息的面板ObjectPanel
+    public TextMeshProUGUI objectName;//显示物体名称objectName
+    public Image KeyIcon;//显示物体是否为钥匙
     [Header("检测距离")]
     public float maxDistance;
-    [Header("物品名称")]
-    public TextMeshProUGUI objectName;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -21,8 +23,7 @@ public class Inspect : MonoBehaviour
     }
     void Start()
     {
-        //获取canvas下的ObjectName中的TextMeshProUGUI组件,用来显示物体名称
-        objectName = canvas.transform.Find("ObjectName").GetComponent<TextMeshProUGUI>();
+
     }
 
     // Update is called once per frame
@@ -33,26 +34,19 @@ public class Inspect : MonoBehaviour
 
     public void  HandleRaycast()
     {
-        objectName.enabled = false;//隐藏物体名称
+        ObjectPanel.gameObject.SetActive(false);//隐藏物体面板
         currentObject = null;//初始化当前物体
         //射线检测
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, maxDistance))
         {
-            //如果检测到物体的标签是Insectable可交互物体,则显示物体名称
+            //如果检测到物体的标签是Insectable可交互物体,则显示物体信息
             if (hit.collider.CompareTag("Inspectable"))
             {
-                currentObject = hit.collider.gameObject;
-                if (objectName != null)
-                {
-                    objectName.enabled = true;
-                    objectName.text = currentObject.GetComponent<InteractiveObject>().Name;//获取当前物体InteractiveObject组件中的Name变量并显示
-                }
-                else
-                {
-                    Debug.Log("objectName/TMPUI组件未获取");
-                }
+                currentObject = hit.collider.gameObject;//设置当前物体
+                ObjectPanel.gameObject.SetActive(true);
+                objectName.text = currentObject.GetComponent<InteractiveObject>().Name;//获取当前物体InteractiveObject组件中的Name变量并显示
             }
         }
     }
